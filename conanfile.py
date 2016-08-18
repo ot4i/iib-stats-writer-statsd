d8f986bf5ae6055e6663c125c026c53768ce91cc
Simon Stone <sstone1@uk.ibm.com>
2016-08-18 05:29:08 +0100

    Switch to conanfile.py to avoid fPIC problems on Windows

---
+++ conanfile.py
@@ -0,0 +1,10 @@
+from conans import ConanFile, CMake
+
+class StatsWriterStatsd(ConanFile):
+  settings = "os", "compiler", "build_type", "arch"
+  requires = "Boost/1.60.0@lasote/stable" # comma separated list of requirements
+  generators = "cmake"
+
+  def config(self):
+    if self.settings.os == "Linux":
+      self.options["Boost"].fPIC = True
